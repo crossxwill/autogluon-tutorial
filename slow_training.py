debb5d4c64fc4127a46c8c095689d72d8e93d018
William Chiu <chiuwilliam@gmail.com>
2024-08-17 20:20:48 -0700

    removed log from slow_training.py

--- slow_training.py
+++ slow_training.py
@@ -1,4 +1,3 @@
-
 from autogluon.tabular import TabularDataset, TabularPredictor
 import numpy as np
 from sklearnex import patch_sklearn
@@ -12,7 +11,7 @@ test_data = TabularDataset(f'{data_url}test.csv')
 
 # train
 np.random.seed(2024)
-predictor = TabularPredictor(label=label, problem_type='multiclass', eval_metric='log_loss', log_to_file=True)
+predictor = TabularPredictor(label=label, problem_type='multiclass', eval_metric='log_loss')
 predictor.fit(train_data, included_model_types=['XGB', 'CAT'])  
 
 # report
